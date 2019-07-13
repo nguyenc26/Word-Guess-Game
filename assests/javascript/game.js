9ec999abb790b5aefc52786cf50907fdfe551db9
Cuong Nguyen <nguyenc72694@gmail.com>
2019-07-12 22:15:15 -0500

    added key event listener

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -4,13 +4,27 @@ const PokemonNames = ["pikachu", "charmander", "squirtle", "bulbasaur"]
 var randomNumber = Math.floor(Math.random() * PokemonNames.length);
 var ChoosenWord = PokemonNames[randomNumber]
 console.log(ChoosenWord);
+var underscores = [];
 
 //guesses left 
 var guessesLeft = 15;
 //guesses that have already made 
 
 //create underscores based on word 
+var generateUnderscore = () => {
+    for (var i = 0; i < ChoosenWord.length; i++) {
+        underscores.push("_");
+    }
+    return underscores;
+    //ask what the return part does again.
+}
+console.log(generateUnderscore());
+
 //listen for user guess 
+document.addEventListener("keypress", (event) => {
+    var keyword = String.fromCharCode(event.keyCode);
+    console.log(keyword);
+});
 //check if guess is right
 //if user is correct we need to show on the under score 
 //if user is wrong it needs to show up on the words already guess 
