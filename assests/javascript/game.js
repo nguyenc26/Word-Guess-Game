b7e587dd1f199f3edfcbaa2a7c3ec3bfe9f87e04
Cuong Nguyen <nguyenc72694@gmail.com>
2019-07-15 11:51:07 -0500

    got the arrays of right and wrong to work

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,10 +1,12 @@
 //Create an array of words
 const PokemonNames = ["pikachu", "charmander", "squirtle", "bulbasaur"]
-//choose the word 
+//choose the word and variables 
 var randomNumber = Math.floor(Math.random() * PokemonNames.length);
 var ChoosenWord = PokemonNames[randomNumber]
-console.log(ChoosenWord);
+var rightWord = [];
+var wrongWord = [];
 var underscores = [];
+console.log(ChoosenWord);
 
 //guesses left 
 var guessesLeft = 15;
@@ -23,15 +25,19 @@ console.log(generateUnderscore());
 //listen for user guess 
 document.addEventListener("keypress", (event) => {
     var keyword = String.fromCharCode(event.keyCode);
-    console.log(keyword);
-});
-//check if guess is right
-if (keyword) {
+    //logs out if the which place the word is in within the array
+    console.log(ChoosenWord.indexOf(keyword));
+    //if the letter is in the array it is true. if not the result will be -1
+    //if user guess is right
+    if (ChoosenWord.indexOf(keyword) > -1) {
+       //adding to right array
+       rightWord.push(keyword);
+       console.log(rightWord);
+       //adding to wrong array 
+    }else{
+       wrongWord.push(keyword);
+       console.log(wrongWord);
 
-} else {
+    }
     
-}
-
-//if user is correct we need to show on the under score 
-//if user is wrong it needs to show up on the words already guess 
-    //also deduct a point of the lives left
+});
